6bef9d43c7e90d5359ad39a77bdac9df783b9415
poojakalvakollu <poojakalvakollu@gmail.com>
2025-09-17 21:52:22 +0530

    Operators

---
+++ Teegala/src/com/tnsif/first/Operator.java
@@ -0,0 +1,16 @@
+package com.tnsif.first;
+
+public class Operator {
+	public static void main(String[] args) {
+		
+		        int a = 10, b = 3;
+		        System.out.println("a + b = " + (a + b));
+		        System.out.println("a - b = " + (a - b));
+		        System.out.println("a * b = " + (a * b));
+		        System.out.println("a / b = " + (a / b));
+		        System.out.println("a % b = " + (a % b));
+		    
+
+	}
+
+}
